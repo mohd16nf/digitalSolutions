e2f8d5c06edac9387c3f9a299c8d1857aa1453e8
mohd16nf <mdasif16nf@gmail.com>
2024-07-13 19:18:40 +0530

    changes

--- app/components/AboutUsSection.jsx
+++ app/components/AboutUsSection.jsx
@@ -3,12 +3,12 @@ import { FaArrowRight } from 'react-icons/fa';
 
 const WelcomeSection = () => {
   return (
-    <div className="mx-auto py-8 xl:px-40 md:py-28" id='about-section'>
-      <h1 className="text-3xl md:text-6xl font-bold mb-8 text-center text-cyan-500">
-      Newest IT/ITes Service Delivery <br /> Company in India.
+    <div className="mx-auto py-8 md:py-28 about-section">
+      <h1 className="text-3xl md:text-5xl font-bold mb-8 text-center text-cyan-500">
+      Newest IT Services Provider <br /> Company in India.
       </h1>
       <h2 className="text-black text-2xl md:text-2xl mb-8 font-bold text-center">
-        Largest IT/ITes Service Delivery Company in India.
+        Largest IT Services Provider Company in India.
       </h2>
       <p className="text-black text-lg md:text-xl mb-16">
       IT Digitals was incorporated, in 2023, with the underlying objectives of
--- app/components/OurTeamMembers.jsx
+++ app/components/OurTeamMembers.jsx
@@ -38,9 +38,9 @@ function Home() {
         
       
 
-      <main className="py-10">
+      <main className="py-10 team-members">
         <h1 className="text-4xl font-bold text-center mb-10">Our Team Members</h1>
-        <div className="flex flex-wrap justify-center xl:gap-12 gap-4">
+        <div className="flex flex-wrap justify-center xl:gap-8 gap-4">
           {teamMembers.map((member, index) => (
             <TeamMemberCard
               key={index}
